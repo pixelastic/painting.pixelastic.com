bb2e39545bfac8c5b4029f89256de02decdcfe40
Tim Carry <tim@pixelastic.com>
2021-04-02 23:20:17 +0200

    Add next/previous posts to post pages

--- src/_data/helper.js
+++ src/_data/helper.js
@@ -2,11 +2,8 @@ const _ = require('golgoth/lodash');
 const path = require('path');
 
 module.exports = {
-  latestPosts(posts) {
-    return _.chain(posts).sortBy('date').reverse().value();
-  },
   findPostByUrl(posts, url) {
     const slug = path.dirname(url.sourceFile);
-    return posts[slug];
+    return _.find(posts, { slug });
   },
 };
--- src/_data/posts.js
+++ src/_data/posts.js
@@ -2,15 +2,23 @@ const glob = require('firost/glob');
 const firostRequire = require('firost/require');
 const pMap = require('golgoth/pMap');
 const readPost = firostRequire('../../lib/readPost.js', { forceReload: true });
+const _ = require('golgoth/lodash');
 
 module.exports = async (options) => {
   const { fromPath } = options;
   const filepaths = await glob(fromPath('*/index.md'));
 
-  const posts = {};
-  await pMap(filepaths, async (filepath) => {
-    const postData = await readPost(filepath);
-    posts[postData.slug] = postData;
+  // Read all posts
+  let posts = await pMap(filepaths, readPost);
+
+  // Sort by date
+  posts = _.chain(posts).sortBy('date').reverse().value();
+
+  // Add previous/next
+  posts = _.map(posts, (post, index) => {
+    post.previousPost = posts[index - 1];
+    post.nextPost = posts[index + 1];
+    return post;
   });
 
   return posts;
