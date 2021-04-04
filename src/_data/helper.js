3bdb7b526c07770cf8b8be8b22e784b68edbc126
Tim Carry <tim@pixelastic.com>
2021-04-04 09:25:21 +0200

    Fix live-reload of .md files

--- src/_data/helper.js
+++ src/_data/helper.js
@@ -1,9 +1,25 @@
 const _ = require('golgoth/lodash');
 const path = require('path');
+const readPost = require('../../lib/readPost.js');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   findPostByUrl(posts, url) {
     const slug = path.dirname(url.sourceFile);
-    return _.find(posts, { slug });
+    const postInCache = _.find(posts, { slug });
+
+    // In prod, we read the local data cache
+    if (isProduction) {
+      return postInCache;
+    }
+
+    // In dev, we re-read from the file, so live-reload correctly updates the
+    // content
+    const filepath = path.resolve(`./src/${url.sourceFile}`);
+    const freshPost = readPost(filepath);
+    return {
+      ...postInCache,
+      ...freshPost,
+    };
   },
 };
