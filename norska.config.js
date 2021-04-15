797e3f28cdd2bd60c7e878e205f863d773d1d02a
Tim Carry <tim@pixelastic.com>
2021-04-15 23:18:03 +0200

    Add feature flags for pagination and tags

--- norska.config.js
+++ norska.config.js
@@ -5,6 +5,9 @@ const getTags = require('./lib/getTags.js');
 const themeConfig = require('./src/_data/theme.js');
 const _ = require('golgoth/lodash');
 
+const ENABLE_PAGINATION = true;
+const ENABLE_TAGS = true;
+
 module.exports = {
   cloudinary: {
     bucketName: 'pixelastic-painting',
@@ -14,23 +17,27 @@ module.exports = {
       const posts = await getPosts();
 
       // Generate all /page-X/ history
-      const postsPerPage = _.get(themeConfig, 'postsPerPage');
-      const maxPage = Math.ceil(posts.length / postsPerPage);
-      await pMap(_.range(2, maxPage + 1), async (pageNumber) => {
-        const template = 'index.pug';
-        const destination = `page-${pageNumber}/index.html`;
-        const data = { hooks: { pageNumber } };
-        await createPage(template, destination, data);
-      });
+      if (ENABLE_PAGINATION) {
+        const postsPerPage = _.get(themeConfig, 'postsPerPage');
+        const maxPage = Math.ceil(posts.length / postsPerPage);
+        await pMap(_.range(2, maxPage + 1), async (pageNumber) => {
+          const template = 'index.pug';
+          const destination = `page-${pageNumber}/index.html`;
+          const data = { hooks: { pageNumber } };
+          await createPage(template, destination, data);
+        });
+      }
 
       // Generate all /tags/xxx pages
-      const tags = await getTags();
-      await pProps(tags, async (tag) => {
-        const template = '_includes/templates/tags.pug';
-        const destination = `${tag.url}index.html`;
-        const data = { hooks: { tag } };
-        await createPage(template, destination, data);
-      });
+      if (ENABLE_TAGS) {
+        const tags = await getTags();
+        await pProps(tags, async (tag) => {
+          const template = '_includes/templates/tags.pug';
+          const destination = `${tag.url}index.html`;
+          const data = { hooks: { tag } };
+          await createPage(template, destination, data);
+        });
+      }
     },
   },
 };
