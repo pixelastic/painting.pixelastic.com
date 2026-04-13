81944b2a7cd6a0a308cac342b1b42a44c20b4525
Tim Carry <tim@pixelastic.com>
2026-04-13 13:13:42 +0200

    chore(deps): update aberlaas to 2.27.3

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,24 +2,5 @@ import config from 'aberlaas/configs/stylelint';
 
 export default {
   ...config,
-  rules: {
-    ...config.rules,
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-          'layer',
-          'import',
-          'plugin',
-          'theme',
-          'source',
-        ],
-      },
-    ],
-  },
+  ignoreFiles: ['assets/style.css', 'public/**/*'],
 };
