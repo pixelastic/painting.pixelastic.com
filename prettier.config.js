4301a62a4e56adfb0aabc711424bf99aa03b10f2
Tim Carry <tim@pixelastic.com>
2026-04-16 00:29:50 +0200

    style(ui): apply Prettier formatting to Hugo templates
    Adds prettier-plugin-go-template and formats all HTML layouts with
    consistent indentation and attribute spacing. Enables automated
    formatting in CI and development workflow.

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,13 @@ import config from 'aberlaas/configs/prettier';
 
 export default {
   ...config,
+  plugins: [...config.plugins, 'prettier-plugin-go-template'],
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        parser: 'go-template',
+      },
+    },
+  ],
 };
